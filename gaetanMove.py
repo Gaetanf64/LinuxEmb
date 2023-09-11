c077c4ce3348776fa2c8b9a9bf815bd6f688ce38
Gaetan <gaetan.fouillet@gmail.com>
2023-09-11 15:58:21 +0200

    fonctionne

--- gaetanMove.py
+++ gaetanMove.py
@@ -2,42 +2,54 @@
 import RPi.GPIO as GPIO
 import time
 
-# Initialisation des LED
+# Initialisation des LED et du capteur
 rouge = 33
 vert = 37
 jaune = 35
+capteur= 38
 
-# Initialisation de le LED verte
+# Initialisation du setup de la LED verte
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(vert, GPIO.OUT,initial = GPIO.LOW)
+GPIO.setup(vert, GPIO.OUT,initial = GPIO.HIGH)
 
-GPIO.output(vert,GPIO.HIGH)
-time.sleep(10)
-
-# Initialisation de le LED jaune
+# Initialisation du setup de la LED jaune
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(jaune, GPIO.OUT,initial = GPIO.LOW)
 
-GPIO.output(jaune,GPIO.HIGH)
-time.sleep(10)
-
-# Initialisation de le LED rouge
+# Initialisation du setup de la LED rouge
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(rouge, GPIO.OUT,initial = GPIO.LOW)
 
-GPIO.output(rouge,GPIO.HIGH)
-time.sleep(10)
+# Initialisation du setup du capteur
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(capteur, GPIO.IN)
 
 
+# Boucle qui fonctionne tant que le programme est exécuté
+while(True):
+    # LED verte active de base
+    GPIO.output(vert, GPIO.HIGH)
 
-GPIO.output(vert,GPIO.LOW)
-GPIO.output(jaune,GPIO.LOW)
-GPIO.output(rouge,GPIO.LOW)
+    # Si le capteur détecte un mouvement
+    if GPIO.input(capteur)==False:
+        # On modifie les LED : la jaune s'allume et la verte s'éteind
+        GPIO.output(vert, GPIO.LOW)
+        GPIO.output(jaune, GPIO.HIGH)
+        time.sleep(0.5)
+        
+        # Tant que le capteur détecte un mouvement continu
+        while GPIO.input(capteur)==False:
+            # On modifie les LED : la rouge s'allume et la jaune s'éteind
+            GPIO.output(jaune, GPIO.LOW)
+            GPIO.output(rouge, GPIO.HIGH)
+            time.sleep(1.5)
+        # Une fois la boucle while terminée, on coupe les LED rouge et jaune
+        GPIO.output(jaune, GPIO.LOW)
+        GPIO.output(rouge, GPIO.LOW)
+    time.sleep(0.05)  # Pause pour ne pas saturer le processeur
 
 
-# state = GPIO.input(LED) #Lit l'état actuel du GPIO, vrai si allumé, faux si éteint
-
-# if state : #Si GPIO allumé
-#     GPIO.output(LED, GPIO.LOW) #On l’éteint
-# else : #Sinon
-#     GPIO.output(LED, GPIO.HIGH) #On l'allume
+# On éteind toutes les LED
+GPIO.output(jaune, GPIO.LOW)
+GPIO.output(rouge, GPIO.LOW)
+GPIO.output(vert, GPIO.LOW)
