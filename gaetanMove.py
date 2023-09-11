c9734934e511a43c2301a2a75ec5ef5e30093d91
Gaetan <gaetan.fouillet@gmail.com>
2023-09-11 16:54:13 +0200

    try catch

--- gaetanMove.py
+++ gaetanMove.py
@@ -27,29 +27,33 @@ GPIO.setup(capteur, GPIO.IN)
 
 # Boucle qui fonctionne tant que le programme est exécuté
 while(True):
-    # LED verte active de base
-    GPIO.output(vert, GPIO.HIGH)
+    try:
+        # LED verte active de base
+        GPIO.output(vert, GPIO.HIGH)
 
-    # Si le capteur détecte un mouvement
-    if GPIO.input(capteur)==False:
-        # On modifie les LED : la jaune s'allume et la verte s'éteind
-        GPIO.output(vert, GPIO.LOW)
-        GPIO.output(jaune, GPIO.HIGH)
-        time.sleep(0.5)
-        
-        # Tant que le capteur détecte un mouvement continu
-        while GPIO.input(capteur)==False:
-            # On modifie les LED : la rouge s'allume et la jaune s'éteind
+        # Si le capteur détecte un mouvement
+        if GPIO.input(capteur)==False:
+            # On modifie les LED : la jaune s'allume et la verte s'éteind
+            GPIO.output(vert, GPIO.LOW)
+            GPIO.output(jaune, GPIO.HIGH)
+            time.sleep(0.5)
+            
+            # Tant que le capteur détecte un mouvement continu
+            while GPIO.input(capteur)==False:
+                # On modifie les LED : la rouge s'allume et la jaune s'éteind
+                GPIO.output(jaune, GPIO.LOW)
+                GPIO.output(rouge, GPIO.HIGH)
+                time.sleep(1.5)
+            # Une fois la boucle while terminée, on coupe les LED rouge et jaune
             GPIO.output(jaune, GPIO.LOW)
-            GPIO.output(rouge, GPIO.HIGH)
-            time.sleep(1.5)
-        # Une fois la boucle while terminée, on coupe les LED rouge et jaune
+            GPIO.output(rouge, GPIO.LOW)
+        time.sleep(0.05)  # Pause pour ne pas saturer le processeur
+    
+    # Si l'utilisateur entre un CTRL + C ou CTRL + Z, une interception est gérée
+    except KeyboardInterrupt:
         GPIO.output(jaune, GPIO.LOW)
         GPIO.output(rouge, GPIO.LOW)
-    time.sleep(0.05)  # Pause pour ne pas saturer le processeur
+        GPIO.output(vert, GPIO.LOW)
+        print("Le programme a été fermé manuellement !")
+        raise SystemExit
 
-
-# On éteind toutes les LED
-GPIO.output(jaune, GPIO.LOW)
-GPIO.output(rouge, GPIO.LOW)
-GPIO.output(vert, GPIO.LOW)
