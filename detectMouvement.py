60d30a1297f910dc42959e994a9623afcf85a505
chrisjeand <wychrisjeand@yahoo.fr>
2023-09-11 14:23:01 +0200

    commit detection OK

--- detectMouvement.py
+++ detectMouvement.py
@@ -1,12 +1,22 @@
 
-# Import des modules
+
+
 import RPi.GPIO as GPIO
 import time
-
+rouge = 33
+jaune= 35
+vert =37
+capteur= 38
 # Initialisation de la numerotation et des E/S
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(37, GPIO.OUT,initial = GPIO.LOW)
+GPIO.setup(jaune, GPIO.OUT)
+GPIO.setup(capteur, GPIO.IN)
+while(True):
+        if GPIO.input(capteur)==False:
+                print("fonctionne")
+        else:
+                print("ne fonctionne pas")
 
 
-GPIO.output(37,GPIO.HIGH)
-time.sleep(10)
+        time.sleep(0.5)
+
